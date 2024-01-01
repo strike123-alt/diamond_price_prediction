e9a20f0afa791100f67aeeeb65aa69b69061e6c3
Paresh Pandya <pareshpandya026@gmail.com>
2024-01-01 21:43:15 +0530

    Deployment V1

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from src.pipeline.prediction_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
- 
+   
+
+   
 @app.route('/')
 def home_page(): 
     return render_template('index.html')
